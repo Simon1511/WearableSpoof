fce5e3eb87487584feecc22f82d0cecb30f25db8
Simon1511 <me@simon1511.de>
2023-05-14 14:36:19 +0200

    app: Hook into Wearable plugins
    Signed-off-by: Simon1511 <me@simon1511.de>

--- app/src/main/java/com/simon1511/wearablespoof/MainHook.java
+++ app/src/main/java/com/simon1511/wearablespoof/MainHook.java
@@ -28,17 +28,62 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class MainHook implements IXposedHookLoadPackage {
 
+    private static final String MANAGER_PACKAGE_NAME = "com.samsung.android.app.watchmanager";
+    private static final String BUDS_PLUS_PACKAGE_NAME = "com.samsung.accessory.popcornmgr";
+    private static final String BUDS_PRO_PACKAGE_NAME = "com.samsung.accessory.atticmgr";
+    private static final String BUDS2_PACKAGE_NAME = "com.samsung.accessory.berrymgr";
+    private static final String BUDS_LIVE_PACKAGE_NAME = "com.samsung.accessory.neobeanmgr";
+    private static final String WATCH4_PACKAGE_NAME = "com.samsung.android.waterplugin";
+    private static final String FIT2_PACKAGE_NAME = "com.samsung.android.neatplugin";
+    private static final String WATCH3_PACKAGE_NAME = "com.samsung.android.gearnplugin";
+    private static final String FIT_PACKAGE_NAME = "com.samsung.android.modenplugin";
+    private static final String WATCH_ACTIVE2_PACKAGE_NAME = "com.samsung.android.gearrplugin";
+    private static final String WATCH_ACTIVE_PACKAGE_NAME = "com.samsung.android.gearpplugin";
+    private static final String GEAR_S_PACKAGE_NAME = "com.samsung.android.gearoplugin";
+    private static final String WATCH5_PACKAGE_NAME = "com.samsung.android.heartplugin";
+    private static final String WATCH_PACKAGE_NAME = "com.samsung.android.geargplugin";
+    private static final String BUDS_MGR_PACKAGE_NAME = "com.samsung.accessory.fridaymgr";
+    private static final String ACCESSORY_SERVICE_PACKAGE_NAME = "com.samsung.accessory";
+
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
-        XposedBridge.log("WearableSpoof: we're behind the scenes!! package name: " + lpparam.packageName);
+        XposedBridge.log("WearableSpoof: Started for: " + lpparam.packageName);
 
-        if (lpparam.packageName.equals("com.samsung.android.app.watchmanager")) {
-            XposedBridge.log("WearableSpoof: Hooking into Watchmanager");
+        if (lpparam.packageName.equals(MANAGER_PACKAGE_NAME) || lpparam.packageName.equals(BUDS_PLUS_PACKAGE_NAME) ||
+                lpparam.packageName.equals(BUDS_PRO_PACKAGE_NAME) || lpparam.packageName.equals(BUDS2_PACKAGE_NAME) ||
+                lpparam.packageName.equals(BUDS_LIVE_PACKAGE_NAME) || lpparam.packageName.equals(WATCH4_PACKAGE_NAME) ||
+                lpparam.packageName.equals(FIT2_PACKAGE_NAME) || lpparam.packageName.equals(WATCH3_PACKAGE_NAME) ||
+                lpparam.packageName.equals(FIT_PACKAGE_NAME) || lpparam.packageName.equals(WATCH_ACTIVE2_PACKAGE_NAME) ||
+                lpparam.packageName.equals(WATCH_ACTIVE_PACKAGE_NAME) || lpparam.packageName.equals(GEAR_S_PACKAGE_NAME) ||
+                lpparam.packageName.equals(WATCH5_PACKAGE_NAME) || lpparam.packageName.equals(WATCH_PACKAGE_NAME) ||
+                lpparam.packageName.equals(BUDS_MGR_PACKAGE_NAME) || lpparam.packageName.equals(ACCESSORY_SERVICE_PACKAGE_NAME)) {
+            XposedBridge.log("WearableSpoof: Hooking into: " + lpparam.packageName);
 
             // ro.product.manufacturer
             if (Build.MANUFACTURER.equals("samsung")) {
-                XposedHelpers.setStaticObjectField(Build.class, "MANUFACTURER", "google");
+                XposedHelpers.setStaticObjectField(Build.class, "MANUFACTURER", "Google");
             }
+
+            // ro.product.brand
+            if (Build.BRAND.equals("samsung")) {
+                XposedHelpers.setStaticObjectField(Build.class, "BRAND", "google");
+            }
+
+            // ro.product.device - always spoof this
+            XposedHelpers.setStaticObjectField(Build.class, "DEVICE", "cheetah");
+
+            // ro.build.tags
+            if (!Build.TAGS.equals("release-keys")) {
+                XposedHelpers.setStaticObjectField(Build.class, "TAGS", "release-keys");
+            }
+
+            // ro.build.type
+            if (!Build.TYPE.equals("user")) {
+                XposedHelpers.setStaticObjectField(Build.class, "TYPE", "user");
+            }
+
+            // ro.product.name - always spoof this
+            XposedHelpers.setStaticObjectField(Build.class, "PRODUCT", "cheetah");
 
             // ro.product.model
             if (Build.MODEL.startsWith("SM-")) {
@@ -50,6 +95,8 @@ public class MainHook implements IXposedHookLoadPackage {
                 XposedHelpers.setStaticObjectField(Build.class, "FINGERPRINT",
                         "google/cheetah/cheetah:13/TQ2A.230505.002/9891397:user/release-keys");
             }
+        } else {
+            XposedBridge.log("WearableSpoof: Not a Samsung Wearable app");
         }
     }
 }
